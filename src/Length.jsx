53467e93e3de50b364e483b83eeaf52445d0c284
Will Baxter <bax.will@googlemail.com>
2024-04-25 10:13:58 +0100

    Completed exercise

---
+++ src/Length.jsx
@@ -0,0 +1,7 @@
+export default function Length ({ name }) {
+  return (
+    <p className="name-length">
+      Did you know that {name} is {name.length} characters long?!
+    </p>
+  );
+}
---
+++ src/NameReversed.jsx
@@ -0,0 +1,5 @@
+export default function NameReversed ({name, reverse}) {
+    return (<p className="name-reversed">
+    Also, {name} backwards is {reverse(name)}
+  </p>)
+}
